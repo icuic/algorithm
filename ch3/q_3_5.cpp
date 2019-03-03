6cd84663d5782780dbe2760ff488ff8769f4d36c
will <icuic@foxmail.com>
2019-03-03 23:20:01 +0800

    q_3_5.cpp

--- ch3/q_3_5.cpp
+++ ch3/q_3_5.cpp
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <algorithm>
 
 #define PUZZLE_SCALE     (5)
-#define MAX_PUZZLE_NUM  (20)
+#define MAX_PUZZLE_NUM  (10)
 #define STEPS_SIZE      (20)
 #define BUFFER_LENGTH   (20)
 
 typedef struct
 {
-    char puzzle[PUZZLE_SCALE][PUZZLE_SCALE];
-    char steps[STEPS_SIZE];
+    char puzzle[PUZZLE_SCALE][PUZZLE_SCALE];    // 题面
+    char steps[STEPS_SIZE];     //  移动步骤
 }stPuzzle;
 
+stPuzzle puzzles[MAX_PUZZLE_NUM] = {0};
+
+using namespace std;
+
 // 找到空格的坐标
-void findSpace(char arr[][PUZZLE_SCALE], char *x, char *y)
+bool findSpace(char arr[][PUZZLE_SCALE], char *x, char *y)
 {
     int i = 0, j = 0;
     
@@ -28,22 +33,22 @@ void findSpace(char arr[][PUZZLE_SCALE], char *x, char *y)
                 *x = i;
                 *y = j;
                 
-                return;
-            }
-            
+                return true;
+            }            
             j++;
-        }
-        
+        }        
         i++;
-    }    
+    }
+
+    return false;
 }
 
-int main()
+char getInput()
 {
     char buffer[BUFFER_LENGTH] = {0};  // 用于临时接收每一行输入的字符
-    stPuzzle puzzles[MAX_PUZZLE_NUM] = {0};
-    char puzzle_id = 0;
-    char i = 0, j = 0;
+    
+    int i = 0, j = 0;
+    char puzzle_id = 0;         // 用于遍历多组题目
     
     // 从标准输入读取所有输入
     for (;;)
@@ -73,7 +78,6 @@ int main()
         
         for (;;)
         {
-            memset(buffer, 0xFF, BUFFER_LENGTH);
             fgets(buffer, BUFFER_LENGTH, stdin);
 
             memcpy(&puzzles[puzzle_id].steps[offset], buffer, strlen(buffer)-1);    // 去掉末尾的\n
@@ -87,100 +91,51 @@ int main()
         }     
     
         puzzle_id++;
-    }
-#if 0
-    for (i  = 0; i < puzzle_id; i++)
-    {
-        for (j = 0; j < PUZZLE_SCALE; j++)
-        {
-            for (int k = 0; k < PUZZLE_SCALE; k++)
-            {
-                printf("%c", puzzles[i].puzzle[j][k]);
-                
-                if (k < PUZZLE_SCALE - 1)
-                    printf(" "); 
-                else
-                    printf("\n");
-            }
-        }
-        
-        printf("%s\n", puzzles[i].steps);
-    }
-#endif
+    }   
 
+    return puzzle_id;
+}
 
-    for (i = 0; i < puzzle_id; i++)
+void processInput(char total)
+{
+    int i = 0;
+    
+    for (i = 0; i < total; i++)
     {
         bool bRet = true;
-        
         char row = 0, column = 0;
-        findSpace(puzzles[i].puzzle, &row, &column);
-        // printf("\n## row = %d, column = %d\n", row, column);
-
-        for (j = 0; j < strlen(puzzles[i].steps); j++)
-        {               
-            switch (puzzles[i].steps[j])
-            {
-                case 'A':
-                    if (row == 0)
-                    {
-                        bRet = false;
-                    }
-                    else                    
-                    {
-                        puzzles[i].puzzle[row][column] = puzzles[i].puzzle[row - 1][column];
-                        puzzles[i].puzzle[row - 1][column] = ' ';
-                        row -= 1;
-                    }
+        
+        if(findSpace(puzzles[i].puzzle, &row, &column))
+        {
+            for (int j = 0; j < strlen(puzzles[i].steps); j++)
+            {   
+                char oldX = row;
+                char oldY = column;
+                
+                switch (puzzles[i].steps[j])
+                {
+                    case 'A':   row -= 1;       break;                        
+                    case 'B':   row += 1;       break;                        
+                    case 'L':   column -= 1;    break;                        
+                    case 'R':   column += 1;    break;                        
+                    case '0':                   break;                        
+                    default:    bRet = false;   break;                    
+                }
+                
+                if (row < 0 || row > PUZZLE_SCALE - 1 || column < 0 || column > PUZZLE_SCALE - 1)
+                {
+                    bRet = false; 
                     break;
-
-                case 'B':
-                    if (row == PUZZLE_SCALE - 1)
-                    {
-                        bRet = false;
-                    }
-                    else                    
-                    {
-                        puzzles[i].puzzle[row][column] = puzzles[i].puzzle[row + 1][column];
-                        puzzles[i].puzzle[row + 1][column] = ' ';
-                        row += 1;
-                    }
-                    break;
-                    
-                case 'L':
-                    if (column == 0)
-                    {
-                        bRet = false;
-                    }
-                    else                    
-                    {
-                        puzzles[i].puzzle[row][column] = puzzles[i].puzzle[row][column - 1];
-                        puzzles[i].puzzle[row][column - 1] = ' ';
-                        column -= 1;
-                    }
-                    break;                    
-
-                case 'R':
-                    if (column == PUZZLE_SCALE - 1)
-                    {
-                        bRet = false;
-                    }
-                    else                    
-                    {
-                        puzzles[i].puzzle[row][column] = puzzles[i].puzzle[row][column + 1];
-                        puzzles[i].puzzle[row][column + 1] = ' ';
-                        column += 1;
-                    }
-                    break; 
-                    
-                case '0':
-                    break;
-                    
-                default:
-                    bRet = false;
-                    break;
+                }
+                else
+                {
+                    swap(puzzles[i].puzzle[row][column], puzzles[i].puzzle[oldX][oldY]);
+                }                                       
             }
-            
+        }
+        else
+        {
+            bRet = false;
         }
         
         printf("Puzzle #%d:\n", i + 1);
@@ -204,10 +159,17 @@ int main()
             }            
         }
         
-        //if (j < PUZZLE_SCALE - 1)
+        if (i < total - 1)
             printf("\n");
 
-    }
+    }    
+}
+
+int main()
+{    
+    char totalPuzzleNum = getInput();
+
+    processInput(totalPuzzleNum);
       
     return 0;
 }
